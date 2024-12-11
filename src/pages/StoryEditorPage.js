015aa18e4f4067ce2b762d3e6c248be98aa0f89c
Eleazar Galope <eleazar.galope@gmail.com>
2024-12-12 00:52:21 +0800

    added document titles

--- src/pages/StoryEditorPage.js
+++ src/pages/StoryEditorPage.js
@@ -81,9 +81,7 @@ const StoryEditorPage = () => {
           setSelectedChapterContent(story.chapters?.[0]?.content || ''); // Set content of the first chapter
           setSelectedChapterTitle(story.chapters?.[0]?.title || ''); // Set title of the first chapter
 
-          useEffect(() => {
             document.title = `Editing ${story.chapters?.[0]?.title} | AIPS`;
-        }, []);
 
           if (!story.threadId) {
             setAiThreadId(null);
